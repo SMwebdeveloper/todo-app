73745c905f58fc84bff87c20217e31a8b4cb9e0c
SMwebdeveloper <samandarwebdeveloper@gamil.com>
2023-03-09 17:34:04 +0500

    vue todo app

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<script setup>
+import { ref, onMounted, computed, watch } from "vue";
+
+const todos = ref([]);
+const name = ref("");
+
+const input_content = ref("");
+const input_category = ref(null);
+
+const todos_asc = computed(() =>
+  todos.value.sort(a, (b) => {
+    return (b.createdAt = b.createdAt);
+  })
+);
+
+const addTodo = () => {
+
+}
+
+watch(name, (newVal) => {
+  localStorage.setItem("name", newVal);
+});
+
+onMounted(() => {
+  name.value = localStorage.getItem("name") || "";
+});
+</script>
+
+<template>
+  <main class="app">
+    <section class="greeting">
+      <h2 class="title">
+        What's up , <input type="text" placeholder="Name here" v-model="name" />
+      </h2>
+    </section>
+
+    <section class="create-todo">
+      <h3>CREATE A TODO</h3>
+
+      <form @submit.prevent="addTodo">
+        <h4>What's on your todo list</h4>
+        <input
+          type="text"
+          placeholder="e.g. make a video"
+          v-model="input_content"
+        />
+        {{ input_content }}
+      </form>
+    </section>
+  </main>
+</template>
